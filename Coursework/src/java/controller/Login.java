c5bac7f5933d6795fbe9e7acc13d6866cf7a3bfe
sashag211 <Sasha2.Gonsalves@live.uwe.ac.uk>
2017-11-28 02:13:45 +0000

    changes made to login

--- Coursework/src/java/controller/Login.java
+++ Coursework/src/java/controller/Login.java
@@ -30,22 +30,23 @@ public class Login extends HttpServlet {
         JDBCBean bean = (JDBCBean) getServletContext().getAttribute("JDBCBean");
         String username = request.getParameter("username");
         String password = request.getParameter("password");
-        
         String errorMessage = "";
         boolean loginValidation = false;
         HttpSession session = request.getSession();
-        
 
         ArrayList<ArrayList<Object>> requestedUserDetail = new ArrayList<>();
 
         //checks if user exists in USERS table
+        //get all info into an array and use to filter admin and user
+        //select("SELECT * FROM USERS WHERE \"id\"='"+user+"' AND \"password\"='" + password + "' AND \"status\"='"+status+"'");
+
         try {
             requestedUserDetail = bean.sqlQueryToArrayList("SELECT * FROM ROOT.USERS WHERE \"id\"='" + username + "'");
         } catch (SQLException ex) {
             System.out.println("SQL Login statement failed to execute! ");
             ex.printStackTrace();
         }
-               
+        
 
         // Checks if the getUsername or password is empty 
         if (username == null || password == null || username.length() == 0 || password.length() == 0) {
